147af1cb2bab405cc9b680b72cc30dc267c7de12
Marco Ribeiro <mribeirodev@gmail.com>
2017-01-06 20:40:31 -0200

    Integrated components with Electron - resolves #5

--- renderer.js
+++ renderer.js
@@ -1,7 +1,18 @@
 
 ;( () => {
   'use strict'
+
+  const appId = "SU0myMIe1AUitLKar0mum8My8RbQ87lEaRjjKDgh";
+  const jsKey = "GtvnNXChRLZRYBbWxNy9fM0LPloMfpYICCtMdJIL";
+  const url = "https://parseapi.back4app.com/";
+  const model = "Professores";
+
   require('angular');
+  const newServer = require('./local_modules/server');
+  const newQuery = require('./local_modules/query');
+
+  const server = newServer(appId, jsKey, url);
+  const query = newQuery(model, server);
 
   angular
   .module('autocompleteApp', [])
@@ -9,17 +20,48 @@
   MainCtrl.$inject = ['$http', '$scope'];
 
   function MainCtrl($http, $scope) {
-
-    $scope.options = [
-      {name: 'Fulano'},
-      {name: 'Cicrano'},
-    ];
+    let cronSearch = undefined;
+    $scope.options = [];
+    const capitalizeFirst = str => {
+      return str[0].toUpperCase() + str.slice(1);
+    }
 
     $scope.select = index => {
       $scope.teacher = $scope.options[index];
       $scope.search = "";
       $scope.options = [];
     }
+
+    $scope.getData = () => {
+      if (cronSearch !== undefined){
+        clearTimeout(cronSearch);
+      }
+      cronSearch = setTimeout(() => {
+        let name = $scope.search;
+        if(name !== undefined){
+          name = capitalizeFirst(name);
+        }
+        query.contains('nome', name);
+        query.find({
+          success: results => {
+            $scope.options = [];
+            results.forEach(result => {
+              let data = {};
+              data.nome = result.get('nome');
+              data.imagem = result.get('imagem');
+              data.curriculo = result.get('curriculo');
+              data.materia = result.get('materia');
+              data.nota = result.get('nota');
+              $scope.options.push(data);
+            });
+          },
+          error: err => {
+            console.log(err);
+            $scope.options = [];
+          }
+        })
+      }, 500);
+    }
   }
-  
+
 })();
